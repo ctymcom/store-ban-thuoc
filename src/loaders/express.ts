b3210e20e55ce929d0089577d0d685b6719aa9bc
Thanh Liem <liembsbs123@gmail.com>
2020-06-16 14:29:43 +0700

    add example document

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -11,6 +11,8 @@ import fs from "fs";
 import { configs } from "../configs";
 import compression from "compression"; // compresses requests
 import router from "../routers";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec, swaggerTheme } from "../configs/swagger";
 
 export default ({ app }: { app: express.Application }) => {
   app.use(cors());
@@ -23,6 +25,17 @@ export default ({ app }: { app: express.Application }) => {
   app.set("view engine", "hbs");
   app.set("views", path.join(__dirname, "/../../public/views"));
   app.use("/public", express.static(path.join(__dirname, "../../public")));
+
+  app.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpec, { customCss: swaggerTheme })
+  );
+  
+  app.get("/swagger.json", function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
 
   app.get("/", function (req: Request, res: Response) {
     res.send("Hello World");
