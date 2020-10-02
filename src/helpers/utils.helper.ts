44a8668b2c471793e92461ff6984ca66b5c3826b
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2020-10-02 15:59:55 +0700

    Update:  - Bổ sung hàm cho util

--- src/helpers/utils.helper.ts
+++ src/helpers/utils.helper.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import path from "path";
+import _ from "lodash";
 
 export class UtilsHelper {
   constructor() {}
@@ -14,5 +15,58 @@ export class UtilsHelper {
       }
     });
     return filelist;
+  }
+  static parsePhone(phone: string, pre: string) {
+    if (!phone) return phone;
+    let newPhone = "" + phone;
+    newPhone = newPhone
+      .replace(/^\+84/i, pre)
+      .replace(/^\+0/i, pre)
+      .replace(/^0/i, pre)
+      .replace(/^84/i, pre);
+
+    return newPhone;
+  }
+
+  static isEmail(email: string) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+  static parseObjectWithInfo(params: { object: any; info: any }) {
+    const { info, object } = params;
+    let encodeData = JSON.stringify(object);
+    encodeData = this.parseStringWithInfo({ data: encodeData, info });
+    try {
+      return JSON.parse(encodeData);
+    } catch (err) {
+      return object;
+    }
+  }
+  static parseStringWithInfo(params: { data: string; info: any }) {
+    const { data, info } = params;
+    let messageText = "" + data;
+    const stringRegex = /{{(.*?)}}/g;
+    messageText = messageText.replace(stringRegex, (m: any, field: string) => {
+      let data = _.get(info, field.trim());
+      if (_.isString(data) || _.isNumber(data)) {
+        data = JSON.stringify(data)
+          .replace(/\\n/g, "\\n")
+          .replace(/\\'/g, "\\'")
+          .replace(/\\"/g, '\\"')
+          .replace(/\\&/g, "\\&")
+          .replace(/\\r/g, "\\r")
+          .replace(/\\t/g, "\\t")
+          .replace(/\\b/g, "\\b")
+          .replace(/\\f/g, "\\f")
+          .replace(/^\"(.*)\"$/g, "$1");
+      } else if (_.isObject(data) || _.isBoolean(data)) {
+        data = `<<Object(${JSON.stringify(data)})Object>>`;
+      }
+      return data || "";
+    });
+    return messageText.replace(
+      /\:\"(?: +)?<<Object\((true|false|[\{|\[].*?[\}|\]])\)Object>>(?: +)?\"/g,
+      ":$1"
+    );
   }
 }
