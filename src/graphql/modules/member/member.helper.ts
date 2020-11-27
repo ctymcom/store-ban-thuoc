d63a3e4dec83ac21ea19a36d78541b7e55c17e20
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2020-11-27 11:41:36 +0700

    Feature: Commission Log

--- src/graphql/modules/member/member.helper.ts
+++ src/graphql/modules/member/member.helper.ts
@@ -1,9 +1,18 @@
-import { IMember } from "./member.model";
+import { IMember, MemberModel } from "./member.model";
 import { TokenHelper } from "../../../helpers/token.helper";
 import { ROLES } from "../../../constants/role.const";
+import { Context } from "../../context";
+import { ErrorHelper } from "../../../base/error";
 
 export class MemberHelper {
   constructor(public member: IMember) {}
+
+  static async fromContext(context: Context) {
+    if (![ROLES.MEMBER].includes(context.tokenData.role)) return null;
+    const member = await MemberModel.findById(context.tokenData._id);
+    if (!member) throw ErrorHelper.permissionDeny();
+    return new MemberHelper(member);
+  }
 
   setActivedAt() {
     if (this.member.activated && !this.member.activedAt) {
