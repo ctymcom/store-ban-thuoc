2917cb85269d442585726942c2b89e22da73a893
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2021-01-06 14:48:33 +0700

    Fix: Drop form api

--- src/routers/apis/index.ts
+++ src/routers/apis/index.ts
@@ -1,7 +1,5 @@
 import express from "express";
 import ExampleRoute from "./example.route";
-import FormRoute from "./form.route";
 const router = express.Router();
 router.use("/example", ExampleRoute);
-router.use("/form", FormRoute);
 export default router;
