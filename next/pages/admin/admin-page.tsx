89984d8ea367a7ee5f89074777b45edf93ef62d7
quangvinhbee <72595494+quangvinhbee@users.noreply.github.com>
2021-01-12 16:07:12 +0700

    update: chart js

--- next/pages/admin/admin-page.tsx
+++ next/pages/admin/admin-page.tsx
@@ -1,10 +1,18 @@
 import { DashboardLayout } from "../../layouts/dashboard-layout";
 import { Card } from '../../components/shared/card/card';
 import { BasicStatistics } from '../../layouts/components/basic-statistics'
+import { ChartMember } from '../../layouts/components/chart-member'
+
 export function AdminPage() {
     return <DashboardLayout>
-        <Card>
+        <Card width={'max-w-4xl'}>
             <BasicStatistics></BasicStatistics>
+        </Card>
+        <Card width={'max-w-md w-1/2 inline-block'}>
+            <ChartMember></ChartMember>
+        </Card>
+        <Card width={'max-w-md w-1/2 inline-block'}>
+            <ChartMember></ChartMember>
         </Card>
 
 
