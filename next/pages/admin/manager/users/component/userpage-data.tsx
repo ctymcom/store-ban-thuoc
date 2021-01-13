21cc65a5c952fba91e447f9f4c74bee1b21b3b98
quangvinhbee <72595494+quangvinhbee@users.noreply.github.com>
2021-01-13 20:05:37 +0700

    update user page list view

---
+++ next/pages/admin/manager/users/component/userpage-data.tsx
@@ -0,0 +1,99 @@
+export const UserpageData = [
+    {
+        name: 'Nguyễn Văn Trường An',
+        nickname: 'Andy Nguyen',
+        email: 'nguyenvantruongan@gmail.com',
+        phone: '0919 659 978',
+        date_create: '12/01/2020',
+        status: true
+    },
+    {
+        name: 'Lâm Quang Vinh',
+        nickname: 'Quang Vinh Bee',
+        email: 'nguyenvantruongan@gmail.com',
+        phone: '0919 659 978',
+        date_create: '12/01/2020',
+        status: false
+    },
+    {
+        name: 'Nguyễn Văn Trường An',
+        nickname: 'Andy Nguyen',
+        email: 'nguyenvantruongan@gmail.com',
+        phone: '0919 659 978',
+        date_create: '12/01/2020',
+        status: true
+    },
+    {
+        name: 'Nguyễn Văn Trường An',
+        nickname: 'Andy Nguyen',
+        email: 'nguyenvantruongan@gmail.com',
+        phone: '0919 659 978',
+        date_create: '12/01/2020',
+        status: true
+    },
+    {
+        name: 'Nguyễn Văn Trường An',
+        nickname: 'Andy Nguyen',
+        email: 'nguyenvantruongan@gmail.com',
+        phone: '0919 659 978',
+        date_create: '12/01/2020',
+        status: true
+    },
+    {
+        name: 'Nguyễn Văn Trường An',
+        nickname: 'Andy Nguyen',
+        email: 'nguyenvantruongan@gmail.com',
+        phone: '0919 659 978',
+        date_create: '12/01/2020',
+        status: true
+    },
+    {
+        name: 'Nguyễn Văn Trường An',
+        nickname: 'Andy Nguyen',
+        email: 'nguyenvantruongan@gmail.com',
+        phone: '0919 659 978',
+        date_create: '12/01/2020',
+        status: true
+    },
+    {
+        name: 'Nguyễn Văn Trường An',
+        nickname: 'Andy Nguyen',
+        email: 'nguyenvantruongan@gmail.com',
+        phone: '0919 659 978',
+        date_create: '12/01/2020',
+        status: true
+    },
+    {
+        name: 'Nguyễn Văn Trường An',
+        nickname: 'Andy Nguyen',
+        email: 'nguyenvantruongan@gmail.com',
+        phone: '0919 659 978',
+        date_create: '12/01/2020',
+        status: true
+    },
+    {
+        name: 'Nguyễn Văn Trường An',
+        nickname: 'Andy Nguyen',
+        email: 'nguyenvantruongan@gmail.com',
+        phone: '0919 659 978',
+        date_create: '12/01/2020',
+        status: true
+    },
+    {
+        name: 'Nguyễn Văn Trường An',
+        nickname: 'Andy Nguyen',
+        email: 'nguyenvantruongan@gmail.com',
+        phone: '0919 659 978',
+        date_create: '12/01/2020',
+        status: true
+    },
+    {
+        name: 'Nguyễn Văn Trường An',
+        nickname: 'Andy Nguyen',
+        email: 'nguyenvantruongan@gmail.com',
+        phone: '0919 659 978',
+        date_create: '12/01/2020',
+        status: true
+    },
+
+]
--- next/pages/admin/manager/users/user-page.tsx
+++ next/pages/admin/manager/users/user-page.tsx
@@ -2,6 +2,7 @@ import { Card } from '../../../../components/shared/card/card';
 import { DashboardLayout } from '../../../../layouts/dashboard-layout';
 import { HeaderUserPage } from './component/header-userpage'
 import { ViewListUserpage } from './component/view-list-userpage';
+import { UserpageData } from './component/userpage-data'
 export function UserPage(props) {
 
     return <>
@@ -9,10 +10,9 @@ export function UserPage(props) {
             <div className="grid max-w-6xl">
                 <Card>
                     <HeaderUserPage />
-                    <ViewListUserpage />
+                    <ViewListUserpage ListUserData={UserpageData} />
                 </Card>
             </div>
         </DashboardLayout>
-
     </>
 }
