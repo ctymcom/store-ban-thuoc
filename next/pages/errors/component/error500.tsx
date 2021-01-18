33861179ba291c355e1e5d886404c10e4a120fc1
quangvinhbee <72595494+quangvinhbee@users.noreply.github.com>
2021-01-18 17:37:14 +0700

    update 500 page

--- next/pages/errors/component/error500.tsx
+++ next/pages/errors/component/error500.tsx
@@ -1,14 +1,14 @@
-import { Icon404Page } from "../../../lib/svg";
+import { Icon404Page, Icon500Page } from "../../../lib/svg";
 
 export function Error500() {
     return <>
         <div className="w-full h-screen bg-gray-200">
             <div className="w-full h-full flex items-center justify-center flex-col">
                 <div className="p-5">
-                    <Icon404Page />
+                    <Icon500Page />
                 </div>
-                <h1 className='text-5xl text-primary-500'>Error 404</h1>
-                <h3 className='text-2xl py-5'>Page Not Found</h3>
+                <h1 className='text-5xl text-primary-500'>Error 500</h1>
+                <h3 className='text-2xl py-5'>Internal Server Error</h3>
                 <div className="btn">
                     <div className="py-3 px-28 bg-primary-500 text-white">
                         Trở về trang chủ
---
+++ pages/_error.tsx
@@ -0,0 +1,14 @@
+import { Error500 } from "../next/pages/errors/component/error500"
+
+export function Error({ statusCode }) {
+    return (<>
+        {statusCode ?
+            <Error500 /> : ""}
+    </>
+    )
+}
+
+Error.getInitialProps = ({ res, err }) => {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : 500
+    return { statusCode }
+}
