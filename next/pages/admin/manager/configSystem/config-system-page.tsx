6b2f664dda6c4ffa8824a171b139d04ec5d44da0
quangvinhbee <72595494+quangvinhbee@users.noreply.github.com>
2021-01-19 10:17:30 +0700

    update chong gian lan

--- next/pages/admin/manager/configSystem/config-system-page.tsx
+++ next/pages/admin/manager/configSystem/config-system-page.tsx
@@ -5,15 +5,27 @@ import { HeaderConfigSystemPage } from './component/header-configSystem-page'
 import { InformationCompany } from './component/information-company';
 import { InformationApp } from './component/information-app';
 import { ConfigAccount } from './component/config-account';
+import { AntiFraud } from './component/anti-fraud';
+import { UserpageData } from '../users/component/userpage-data';
+
 export function ConfigSystemPage(props) {
     const [Session, setSession] = useState(1);
+    var breadcrumbs = {}
     var Session_view
     if (Session == 1) {
         Session_view = <InformationCompany />
+        breadcrumbs = {
+            [
+                { title: "Quản trị", path: '/admin/manager' },
+                { title: "Cấu hình hệ thống", path: '/admin/manager/configsystem' }
+            ]
+        }
     } else if (Session == 2) {
         Session_view = <InformationApp />
     } else if (Session == 3) {
         Session_view = <ConfigAccount />
+    } else if (Session == 4) {
+        Session_view = <AntiFraud ListUserData={UserpageData} />
     }
     return <>
         <DashboardLayout
---
+++ pages/admin/manager/configsytem/create-rule.tsx
@@ -0,0 +1,4 @@
+export function CreateRule() {
+    return <>
+    </>
+}
