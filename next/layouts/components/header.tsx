69a36e3e1e0f539af235c582f53a8dafbd34aea2
quangvinhbee <72595494+quangvinhbee@users.noreply.github.com>
2021-01-25 22:20:09 +0700

    update: header footer responesive

--- next/layouts/components/header.tsx
+++ next/layouts/components/header.tsx
@@ -5,6 +5,8 @@ import { IconSearch } from "../../lib/svg/icon-search";
 import { Logo } from "../../lib/svg/logo";
 import { BreadcrumbItem } from "../dashboard-layout";
 import { IconArrowRight } from "../../lib/svg/icon-arrow-right";
+import { IconMenu } from "../../lib/svg/icon-menu";
+import { IconClose } from "../../lib/svg/icon-close";
 type HeaderProps = {
   [x: string]: any;
   breadcrumbs?: BreadcrumbItem[];
@@ -13,11 +15,13 @@ type HeaderProps = {
 export function Header({ activeMenu = 0, breadcrumbs, ...props }: HeaderProps) {
   const topMenu = ["Tin tức", "Tuyển dụng", "Trở thành nhà bán thuốc"];
   const mainMenu = ["Sản phẩm", "Hoạt chất", "Khuyến mãi", "Tin tức sức khỏe"];
+  const NavMenu = ["Sản phẩm", "Hoạt chất", "Khuyến mãi", "Tin tức sức khỏe", 'Tuyển dụng', 'Tài khoản'];
   const [ActiveMenu, setActiveMenu] = useState(activeMenu);
+  const [showNavMenu, setshowNavMenu] = useState(false);
   return (
     <>
-      <div className="w-full">
-        <div className="container-1 flex items-center py-2 bg-gray-200 justify-end text-xs">
+      <div className="w-full hidden md:block">
+        <div className="hidden container-1 md:flex items-center py-2 bg-gray-200 justify-end text-xs">
           {topMenu.map((m, index) => (
             <p key={index} className="text text-gray-400 cursor-pointer ml-5">
               {m}
@@ -25,10 +29,11 @@ export function Header({ activeMenu = 0, breadcrumbs, ...props }: HeaderProps) {
           ))}
         </div>
         <div className="container-1 py-6 grid grid-cols-4">
+          <div className="hidden"></div>
           <div className="w-16 ">
             <Logo />
           </div>
-          <div className="search col-span-2 flex items-center w-full">
+          <div className="hidden search col-span-2 md:flex items-center w-full">
             <div className="relative flex items-center w-full">
               <input
                 type="text"
@@ -40,7 +45,7 @@ export function Header({ activeMenu = 0, breadcrumbs, ...props }: HeaderProps) {
               </div>
             </div>
           </div>
-          <div className="flex items-center justify-end text-gray-500">
+          <div className="hidden md:flex items-center justify-end text-gray-500">
             <div className=" px-3 cursor-pointer">Đăng nhập</div>
             <div className="border-l border-gray-300 px-3 flex items-center cursor-pointer">
               <div className="pr-2">Giỏ hàng</div>
@@ -83,8 +88,65 @@ export function Header({ activeMenu = 0, breadcrumbs, ...props }: HeaderProps) {
             }
           </div>
         }
-
       </div>
+      {/* do header có nhiều thay đổi so với desktop nên em tạo thêm 1 cái mới luôn ạ */}
+
+      <div className="md:hidden w-full flex justify-between  items-center px-4 py-4" >
+        <div className="text-primary-500">
+          <div className="w-6 " onClick={() => { setshowNavMenu(true) }}>
+            <IconMenu />
+          </div></div>
+        <div className="">
+          <div className="w-16 ">
+            <Logo />
+          </div>
+        </div>
+        <div className="flex space-x-4 text-primary-500">
+          <div className="w-6 ">
+            <IconSearch />
+          </div>
+          <div className="w-6 ">
+            <IconCart />
+          </div>
+        </div>
+      </div>
+      <div className={"transition duration-700 min-h-screen h-full w-full fixed top-0 z-50  " + (showNavMenu ? " flex translate-x-4" : "hidden")}>
+        <div className=" w-4/6 bg-white">
+          <div className="py-5 px-5 h-32 bg-primary-500 text-white flex items-center relative">
+            <p className="text-lg">Đăng nhập</p>
+            <div className="w-4 absolute top-5 right-5" onClick={() => { setshowNavMenu(false) }}>
+              <IconClose />
+            </div>
+          </div>
+          <div className="flex flex-col justify-between h-5/6">
+            <div className="">
+              <ul className="inline">
+                {NavMenu.map((item, index) => (
+                  <li
+                    key={index}
+                    className={
+                      "cursor-pointer py-4 px-6 border-b-2 border-gray-100 hover:bg-primary-500 flex justify-between"
+                    }
+                  >
+                    <p className="">{item}</p>
+                    <div className="w-5 text-gray-200"><IconArrowRight /></div>
+                  </li>
+                ))}
+              </ul>
+              <div className="p-5">
+                <div className="px-4 py-3 text-center rounded-full text-sm text-white bg-primary-500">Trở thành nhà cung cấp</div>
+              </div>
+            </div>
+            <div className="p-5">
+              <h2 className="uppercase text-primary-500">hotline miễn phí (7h-22h)</h2>
+              <p className="">Gọi đặt hàng: 1800 6821</p>
+              <p className="">Gọi tư vấn - hỗ trợ: 1800 2001</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-2/6 bg-black opacity-25"></div>
+      </div>
+
     </>
   );
 }
