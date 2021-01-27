ac43c9d17f3bb6e4042652040919eee2e5523cfe
Nin <anthuylinh999@gmail.com>
2021-01-27 16:00:10 +0700

    add:completed folder complete-page; completepng

---
+++ next/pages/completed/completed-page.tsx
@@ -0,0 +1,8 @@
+import { DashboardLayout } from "../../layouts/dashboard-layout";
+import { Completed } from './component/completed';
+
+export default function CompletedPage() {
+    return <DashboardLayout>
+        <Completed title="compl" />
+    </DashboardLayout>
+}
