6a40904ac779f2859be143bc35d95aa33e29bb8e
Nin <anthuylinh999@gmail.com>
2021-02-05 09:56:20 +0700

    fix: Chỉnh sủa giao diện checkout-hover option

--- next/components/index/checkout/checkout-page.tsx
+++ next/components/index/checkout/checkout-page.tsx
@@ -21,52 +21,63 @@ export function CheckOutPage() {
     }
     const router = useRouter()
     return <>
-        <CartPayHeader title="pay" />
-        <div className="w-10/12 m-auto flex justify-between">
-            <div className="w-3/4">
-                <FormCheck title={listFormCheckTrans.tit} checkList={listFormCheckTrans.list} />
-                <FormCheck title={listFormCheckPayment.tit} checkList={listFormCheckPayment.list} />
-                <p className="text-sm">Giảm 3% cho đơn hàng chuyển khoản trước</p>
-                <button className={isHide ? "hidden" : "text-primary"} onClick={() => setIsHide(true)}>Xem thông tin chuyển khoản</button>
-                <TransferInformation info={transferInformation} isHide={isHide} setIsHide={setIsHide} />
-                <h3 className="uppercase mt-10 text-lg">Ghi chú khác</h3>
-                <p>Trường hợp không tìm được thuốc như mong muốn. Quý khách vui lòng điền yêu cầu vào bên dưới. Chúng tôi sẽ</p>
-                <input type="" className="w-full border-2 border-gray-300 rounded-md pl-3 pt-2 pb-14 text-lg my-6" placeholder="Nhập ghi chú của bạn" />
-
-                <div className="flex items-center w-1/3 justify-evenly">
-                    <div className="relative w-4 h-4">
-                        <input type="checkbox" checked={isCheck} onChange={(e) => setIsCheck(e.target.checked)} className="absolute top-0 left-0 appearance-none h-4 w-4 border border-gray-300 rounded-md checked:bg-white border-transparent focus:outline-none" />
-                        <div className={isCheck ? "absolute top-0.5 left-0.5" : "hidden"}><IconCheck /></div>
-                    </div>
-                    <p>Tôi đồng ý với</p>
-                    <button className="text-primary">Điều khoản sử dụng</button>
-                </div>
-                <button onClick={() => router.push('/complete')}
-                    className="w-2/5 border border-gray-300 rounded p-2 mt-2 bg-primary text-white">Xác nhận đặt hàng</button>
-                <p className="text-xs p-2">(Xin vui lòng kiểm tra lại đơn hàng trước khi mua)</p>
+        <div className="container-1">
+            <div>
+                <CartPayHeader title="pay" />
             </div>
-            <div className="w-1/4 ml-10">
-                <div className="mb-12">
-                    <div className="flex justify-between items-center border-b-2 mt-4 pb-2">
-                        <div className="flex items-center">
-                            <i className="text-primary text-24"><IoLocationSharp /></i>
-                            <h4 className="uppercase pl-2">Địa chỉ giao hàng</h4>
+            <div className="">
+                <div className="col-span-3 grid grid-cols-7 gap-20">
+                    <div className="col-span-5 grid grid-cols-5 gap-4">
+                        <div className="col-span-5">
+                            <FormCheck title={listFormCheckTrans.tit} checkList={listFormCheckTrans.list} setIsHide={setIsHide} />
+                            <FormCheck title={listFormCheckPayment.tit} checkList={listFormCheckPayment.list} setIsHide={setIsHide} />
                         </div>
-                        <button className="text-primary btn">Đổi</button>
+                        <div className="col-span-5">
+                            <p className="text-sm col-span-2">Giảm 3% cho đơn hàng chuyển khoản trước</p>
+                            <div>
+                                {
+                                    isHide ? <TransferInformation info={transferInformation} /> : <div></div>
+                                }
+                            </div>
+                        </div>
+                        <div className="col-span-5">
+                            <h3 className="uppercase my-5 text-lg">Ghi chú khác</h3>
+                            <p>Trường hợp không tìm được thuốc như mong muốn. Quý khách vui lòng điền yêu cầu vào bên dưới. Chúng tôi sẽ</p>
+                            <input type="" className="w-full border-2 border-gray-300 rounded-md pl-3 pt-2 pb-14 text-lg my-6" placeholder="Nhập ghi chú của bạn" />
+                        </div>
+                        <div className="col-span-5">
+                            <div className="relative w-4 h-4">
+                            </div>
+                            <p>Tôi đồng ý với</p>
+                            <button className="text-primary">Điều khoản sử dụng</button>
+                        </div>
+                        <button onClick={() => router.push('/complete')}
+                            className="col-span-2 btn mt-2 btn-primary">Xác nhận đặt hàng</button>
+                        <p className="text-xs p-2 col-span-5">(Xin vui lòng kiểm tra lại đơn hàng trước khi mua)</p>
                     </div>
-                    <p className="text-primary">Minh Đức Vi</p>
-                    <p>1111111111</p>
-                    <p>43 Hoa Huệ, Phường 7, Quận Phú</p>
-                </div>
-                <div className="border-b-2 pb-2">
-                    <PayMoney listMoney={listMoneyCheckout} />
-                </div>
-                <div className="flex justify-between pt-2">
-                    <p>Thành tiền</p>
-                    <p className="font-bold text-primary">{NumberPipe(totalMonney(listMoneyCheckout), false)} VND</p>
+                    <div className="col-span-2">
+                        <div className="mb-10">
+                            <div className="grid grid-cols-12 gap-4 border-b-2 items-center">
+                                <i className="text-primary text-16 col-span-1"><IoLocationSharp /></i>
+                                <h4 className="uppercase col-span-8">Địa chỉ giao hàng</h4>
+                                <button className="text-primary font-normal text-left btn col-span-3">Đổi</button>
+                            </div>
+                            <div className="my-2">
+                                <p className="text-18 font-bold">Minh Đức Uy</p>
+                                <p>0914357862</p>
+                                <p>43 Hoa Huệ, Phường 7, Quận Phú</p>
+                            </div>
+                        </div>
+                        <div className="border-b-2 pb-2">
+                            <PayMoney listMoney={listMoneyCheckout} />
+                        </div>
+                        <div className="flex justify-between pt-2">
+                            <p>Thành tiền</p>
+                            <p className="font-bold text-primary">{NumberPipe(totalMonney(listMoneyCheckout), false)} VND</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-
+        </div >
     </>
 }
--- next/components/index/checkout/component/form-check.tsx
+++ next/components/index/checkout/component/form-check.tsx
@@ -18,32 +18,34 @@ export function FormCheck(props) {
             return false;
         return true;
     }
-    const setStyleCheck = (id, type) => {
+    const setStyleCheck = (id, type, title) => {
         switch (type) {
             case "bo": {
-                let tempStyle = "p-4 mr-4 border rounded w-2/5"
+                let tempStyle = "p-2 border rounded col-span-2 hover:border-primary hover:bg-primary-light transition duration-500 ease-in-out"
                 if (id === IDC)
                     tempStyle += " border-primary bg-green-50";
                 return tempStyle;
             }
             case "he": {
-                let tempStyle = "text-lg"
-                if (id === IDC)
+                let tempStyle = ""
+                if (id === IDC) {
                     tempStyle += " text-primary";
+                    if (title = "Chuyển khoản") { props.setIsHide(true); } else { props.setIsHide(false) }
+                }
                 return tempStyle;
             }
         }
     }
-    return <div className="my-6">
-        <h3 className="py-1.5 my-3 uppercase border-b-2 text-xl">{title}</h3>
-        <div className="flex">
+    return <div className="col-span-5">
+        <h3 className="uppercase border-b-2 mb-4">{title}</h3>
+        <div className="grid grid-cols-5 gap-4">
             {
                 checkList.map((item, index) => {
-                    return <div className={setStyleCheck(index, "bo")} key={index} onClick={() => { setIDChecked(index) }}>
+                    return <div className={setStyleCheck(index, "bo", item.title)} key={index} onClick={() => { setIDChecked(index) }}>
                         <div className="flex items-center">
                             <Checkbox checked={setCheckBox(index)} />
                             <div>
-                                <h4 className={setStyleCheck(index, "he")}>{item.title}</h4>
+                                <h4 className={setStyleCheck(index, "he", item.title)}>{item.title}</h4>
                                 <p className="text-gray-400 text-sm">{item.content}</p>
                             </div>
                         </div>
