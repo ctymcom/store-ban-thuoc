1a5dd999a3645286d50511a757a708d2b4d27451
Nin <nnninh.17ck1@gmail.com>
2021-02-22 17:13:05 +0700

    update: breadcrum cart-checkout-complete

--- next/components/index/completed/completed-page.tsx
+++ next/components/index/completed/completed-page.tsx
@@ -1,10 +1,10 @@
 import { CartPayHeader } from '../cart/component/cart-pay-header';
 import { ContentStatus } from './component/content-status';
-import { statusSuccessed, statusFailed } from './component/completed-data';
-export function CompletedPage(props) {
+import { statusSuccessed } from './component/completed-data';
+export function CompletedPage() {
     const status = statusSuccessed;
     return <div className="main-container pb-0">
-        <CartPayHeader/>
+        <CartPayHeader name="complete"/>
         <div className="md:flex md:w-3/4 mx-auto mt-10">
             <div className="w-1/3 lg:w-1/2 mx-auto">
                 <img src={status.img} alt="" />
