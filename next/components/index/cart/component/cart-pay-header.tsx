ba1b81d7a3fb00a6d3b9910d06550553c9377325
Nin <nnninh.17ck1@gmail.com>
2021-02-23 11:04:13 +0700

    update: cart-pay-header, checkout-page

--- next/components/index/cart/component/cart-pay-header.tsx
+++ next/components/index/cart/component/cart-pay-header.tsx
@@ -41,8 +41,6 @@ export function CartPayHeader(props:Proptype) {
     const styleSteps = (id:string)=>{
         let style ="flex justify-around items-center cursor-pointer ";
         let index = listStep.findIndex(item=>item.name===id);
-        console.log(listStep[index]);
-        
         if(listStep[index].actv){    
             style+=" text-gray-600 hover:text-primary";
         }
@@ -51,7 +49,7 @@ export function CartPayHeader(props:Proptype) {
         }
         return style;
     }
-    return <div className="text-sm sm:text-xl md:text-2xl lg:text-3xl w-5/6 sm:w-4/5 md:w-3/5 lg:w-2/5 mx-auto my-10 text-center uppercase sm:whitespace-nowrap text-gray-300 flex items-center">
+    return <div className="text-sm sm:text-xl md:text-2xl lg:text-40 w-5/6 sm:w-4/5 md:w-3/5 lg:w-2/5 mx-auto my-10 text-center uppercase sm:whitespace-nowrap text-gray-300 flex items-center">
             <h2 className={styleSteps("cart")} onClick={() => listStep[0].actv? router.push('/cart'):""}> Giỏ Hàng</h2>
             <h2 className={styleSteps("checkout")} onClick={() => listStep[1].actv? router.push('/checkout'):""}><i><HiOutlineChevronRight/></i>Thanh Toán</h2> 
             <h2 className={styleSteps("complete")} onClick={() => listStep[2].actv? router.push('/complete'):""}><i><HiOutlineChevronRight/></i>Hoàn tất</h2>
--- next/components/index/cart/component/pay-money.tsx
+++ next/components/index/cart/component/pay-money.tsx
@@ -1,16 +1,23 @@
 import { BiMoney } from 'react-icons/bi'
 import { NumberPipe } from '../../../../lib/pipes/number';
-export function PayMoney(props) {
+interface PropsType extends ReactProps{
+    listMoney:ItemCart[],
+}
+type ItemCart={
+    title:string,
+    money:number,
+}
+export function PayMoney(props:PropsType) {
     const { listMoney } = props;
     return <>
-        <div className="flex border-b-2 py-1.5 items-center">
+        <div className="flex border-b-4 p-1 items-center">
             <i className="text-primary text-18"><BiMoney /></i>
-            <p className="uppercase px-2">Thành Tiền</p>
+            <p className="uppercase px-2 text-24">Thành Tiền</p>
         </div>
         <div className="my-2">
             {
                 listMoney.map((item, index) => {
-                    return <div className="flex justify-between" key={index}>
+                    return <div className="flex justify-between text-20" key={index}>
                         <p>{item.title}</p>
                         <p className="font-bold">{NumberPipe(item.money, false)} <span> VND</span></p>
                     </div>
