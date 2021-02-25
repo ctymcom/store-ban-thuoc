42a916bba176f919ff4e01db707387ca1b14d0f0
ThanhNam2905 <thanhnam290596@gmail.com>
2021-02-25 17:13:10 +0700

    Fix: responsive trang reward-point

--- next/components/index/reward-point/reward-point-page.tsx
+++ next/components/index/reward-point/reward-point-page.tsx
@@ -8,11 +8,11 @@ export function RewardPointPage () {
                 <div className="">
                     <div className="w-full flex justify-between mt-0 my-28">
                         <div className="w-full md:w-full">
-                            <div className="flex flex-col md:flex-row items-center justify-center md:justify-start mb-0 md:mb-10 mt-6 md:mt-4 lg:mt-0">
-                                <img className="object-contain mr-0 md:mr-7 w-3/6 sm:w-2/5 md:w-1/4" src="/assets/img/reward-point.png" alt=""/>
+                            <div className="flex flex-col md:flex-row items-center justify-center md:justify-start mb-0 md:mb-10 mt-0 md:mt-4 lg:mt-0">
+                                <img className="object-contain mr-0 md:mr-7 w-3/5 sm:w-1/3 lg:w-1/4 xl:w-1/6" src="/assets/img/reward-point.png" alt=""/>
                                 <div className="text-center -mt-2">
-                                    <p className="text-md xs:text-2xl md:text-lg font-extralight text-gray-700 mt-5">Số Điểm Bạn Đang Có</p>
-                                    <p className="text-xl xs:text-3xl md:text-4xl font-medium text-yellow-400">165.520 Điểm</p>
+                                    <p className="text-md md:text-lg lg:text-xl lg:font-extralight text-gray-700 lg:mb-1.5 lg:-mt-3 xl:-mt-8 lg:text-left">Số Điểm Bạn Đang Có</p>
+                                    <p className="text-xl md:text-2xl lg:text-5xl xl:text-4xl lg:font-extralight text-yellow-400">165.520 Điểm</p>
                                 </div>
                             </div>
                             <ul className="hidden md:flex border-b-4 pb-1 pl-0">
