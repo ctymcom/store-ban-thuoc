02d4464a964fd055975f54089b03e17e7d32275e
Nin <nnninh.17ck1@gmail.com>
2021-03-02 09:49:24 +0700

    fix: chinh giao dien va reponsive profile user

--- next/components/index/profile-user/components/form-profile.tsx
+++ next/components/index/profile-user/components/form-profile.tsx
@@ -37,9 +37,9 @@ export function FormProfile() {
     return <>
     {
         user ? <>
-            <div className="w-full lg:w-5/6 mx-auto xl:ml-40 text-16 sm:text-20 text-gray-800 mt-10">
-                <h3 className="uppercase border-gray-200 border-b-4 pb-2 mb-4 text-24 text-center sm:text-left">Thông tin tài khoản</h3>
-                <div className="flex flex-wrap-reverse justify-between w-full">
+            <div className="w-11/12 lg:w-5/6 mx-auto xl:ml-40 text-16 sm:text-20 text-gray-700 mt-5 sm:mt-10">
+                <h3 className="uppercase border-gray-200 border-b-4 pb-2 mb-4 text-24 hidden sm:block text-left">Thông tin tài khoản</h3>
+                <div className="flex flex-wrap-reverse w-full">
                     <div className="w-full xl:w-3/5 items-center">
                         <div className="pr-0 xl:pr-16 xl:border-r border-gray-400">
                             <div className="sm:flex justify-between items-center">
@@ -77,13 +77,13 @@ export function FormProfile() {
                             <button className="btn-primary w-full sm:w-3/4 font-normal h-12 text-16 sm:text-20">Cập Nhật</button>
                         </div>
                     </div>
-                    <div className="text-center w-full xl:w-2/5 flex items-center xl:block">
-                        <div className="xl:p-4 flex items-center justify-around w-1/4 xl:w-full">
-                            <img src={ user.imageLink || "/assets/img/avatar.svg"} onError={(e) => {(e.target as any).src="/assets/img/avatar.svg"}} className="w-1/3 rounded-full" alt="avatar" />
+                    <div className="flex xl:inline-block w-full xl:w-2/5 justify-around items-center">
+                        <div className="w-1/3 mx-auto">
+                            <img src={ user.imageLink || "/assets/img/avatar.svg"} onError={(e) => {(e.target as any).src="/assets/img/avatar.svg"}} className="w-11/12 rounded-full" alt="avatar" />
                         </div>
-                        <div className="w-full sm:w-3/4 xl:w-full">
-                            <button className="btn-outline text-16 sm:text-20 border-primary border font-normal text-primary w-2/3 mx-auto h-12 my-2">Đổi ảnh</button>
-                            <p className="text-12 whitespace-nowrap sm:text-16 text-gray-400">Dung lượng file tối đa 1MB. <span className="block sm:inline xl:block">Định dạng: .JPEG, .PNG</span></p>
+                        <div className="w-2/3 mx-auto flex items-center flex-wrap">
+                            <button className="sm:mx-auto w-2/3 sm:h-12 my-2 btn-outline text-16 sm:text-20 border-primary border font-normal text-primary hover:bg-primary hover:text-white">Đổi ảnh</button>
+                            <p className="mx-auto text-12 whitespace-nowrap sm:text-16 text-gray-400 hidden sm:inline-block">Dung lượng file tối đa 1MB. <span className="block sm:inline xl:w-11/12 xl:block mx-auto">Định dạng: .JPEG, .PNG</span></p>
                         </div>
                     </div>
                 </div>
