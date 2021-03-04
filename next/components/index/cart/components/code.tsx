bc59aac223de472a665a3185513cab48ec87af95
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2021-03-04 13:12:05 +0700

    Fix: Kết nối dữ liệu khuyến mãi

--- next/components/index/cart/components/code.tsx
+++ next/components/index/cart/components/code.tsx
@@ -1,40 +1,50 @@
-import React from 'react';
-import { useState } from 'react';
-import { Dialog } from '../../../shared/utilities/dialog/dialog';
-const Code = (props) => {
-    const sendCode=()=>{
-        props.choseCode(props.item.code);
-    }
-    const [showMore, setShowMore] = useState(false);
-    const setShow=(e)=>{
-        if (!e) 
-         e = window.event;
-        e.cancelBubble = true;
-        if (e.stopPropagation) e.stopPropagation();
-        setShowMore(!showMore)
-    }
-    return (<>
-        <div className="flex items-center justify-around shadow-md rounded-md m-2 border-2 border-white hover:border-primary hover:text-primary transition-all duration-300 ease-in-out"
-             onClick={()=>sendCode()}>
-            <div className="w-20 border-dashed border-r-2 p-2">
-                <img src="/assets/img/logo.png" alt="logo"/>
-            </div>
-            <div className="p-2 cursor-pointer">
-                <h3 className="text-lg">{props.item.content}</h3>
-                <p className="text-gray-400">Hạn dùng đến: {props.item.expDate}</p>
-                <p className="cursor-pointer text-primary w-16 whitespace-nowrap rounded-md hover:bg-info-light" onClick={(e)=>setShow(e)}>Điều kiện</p>
-            </div>
+import React from "react";
+import { useState } from "react";
+import { Promotion } from "../../../../lib/repo/promotion.repo";
+import { Dialog } from "../../../shared/utilities/dialog/dialog";
+import { usePromotionContext } from "../providers/promotion-provider";
+type CodeProps = {
+  [x: string]: any;
+  item: Promotion;
+};
+const Code = (props: CodeProps) => {
+  const { setSelectedPromotion } = usePromotionContext();
+  const sendCode = () => {
+    setSelectedPromotion(props.item);
+    props.choseCode(props.item.code);
+  };
+  const [showMore, setShowMore] = useState(false);
+  const setShow = (e) => {
+    if (!e) e = window.event;
+    e.cancelBubble = true;
+    if (e.stopPropagation) e.stopPropagation();
+    setShowMore(!showMore);
+  };
+  return (
+    <>
+      <div
+        className="flex items-center justify-around shadow-md rounded-md m-2 border-2 border-white hover:border-primary hover:text-primary transition-all duration-300 ease-in-out"
+        onClick={() => sendCode()}
+      >
+        {/* <div className="w-20 min-w-20 border-dashed border-r-2 p-2 h-20 min-h-20">
+          <img src="/assets/img/logo.png" alt="logo" />
+        </div> */}
+        <div className="p-2 cursor-pointer">
+          <h3 className="text-lg">{props.item.name}</h3>
+          <p className="text-gray-400">{props.item.description}</p>
+          {/* <p className="cursor-pointer text-primary w-16 whitespace-nowrap rounded-md hover:bg-info-light" onClick={(e)=>setShow(e)}>Điều kiện</p> */}
         </div>
-        
-         <Dialog width="410px" isOpen={showMore}
-            onClose={() => setShowMore(false)}
-        >
-            <Dialog.Body>
-                    <p className="m-2" onClick={()=>setShowMore(false)}>{props.item.condition}</p>
-            </Dialog.Body>
-        </Dialog>
-        </>
-    );
-}
+      </div>
+
+      <Dialog width="410px" isOpen={showMore} onClose={() => setShowMore(false)}>
+        <Dialog.Body>
+          <p className="m-2" onClick={() => setShowMore(false)}>
+            {props.item.condition}
+          </p>
+        </Dialog.Body>
+      </Dialog>
+    </>
+  );
+};
 
 export default Code;
