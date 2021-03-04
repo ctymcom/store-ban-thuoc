5d9065e7b476ba45d337cbd118e3de69959f78b0
Link Thai <linkthai1995@gmail.com>
2021-03-04 17:19:56 +0700

    FIX: build error

--- next/components/index/product-details/components/product-content.tsx
+++ next/components/index/product-details/components/product-content.tsx
@@ -1,36 +1,42 @@
-
-import { useProductDetailsContext } from '../providers/product-details-provider';
+import { useProductDetailsContext } from "../providers/product-details-provider";
 
 export function ProductContent() {
+  const { product } = useProductDetailsContext();
+  const scrollTo = (id) => {
+    document
+      .getElementById("tab-" + id)
+      .scrollIntoView({ behavior: "smooth", block: "nearest", inline: "center" });
+    setTimeout(() => {
+      document.getElementById(id).scrollIntoView({ behavior: "smooth", block: "start" });
+    }, 300);
+  };
 
-    const { product } = useProductDetailsContext()
-    const scrollTo = (id) => {  
-        document.getElementById('tab-' + id).scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' });
-        setTimeout(() => {
-            document.getElementById(id).scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }, 300)
-    }
-
-    return <>
-        <div className="flex w-full no-scrollbar bg-white sticky top-16 pt-2 md:top-32 z-10 md:pt-4">
-        {
-            product.tabs.map((tab, index) => 
-                    <a key={tab.name} id={'tab-' + tab.name}
-                     onClick={() => scrollTo(tab.name)}
-                        className="flex-grow text-center px-3 pb-2 font-semibold text-primary hover:text-primary cursor-pointer border-b-2 whitespace-nowrap">{tab.name}
-                    </a>
-            )
-        }
-        </div>
-        <div>
-        {
-            product.tabs.map(tab => <div className="relative mt-7" key={tab.name}>
-                    <div className="absolute -top-36" id={tab.name}></div>
-                    <h3 className="font-bold text-lg leading-9">{tab.name}</h3>
-                    <p className="whitespace-pre-wrap" dangerouslySetInnerHTML={{ __html: tab.content }}></p>
-                </div>
-            )
-        }
-        </div>
-    </>;
-}   
+  return (
+    <>
+      <div className="flex w-full no-scrollbar bg-white sticky top-16 pt-2 md:top-32 z-10 md:pt-4">
+        {product.tabs.map((tab, index) => (
+          <a
+            key={tab.name}
+            id={"tab-" + tab.name}
+            onClick={() => scrollTo(tab.name)}
+            className="flex-grow text-center px-3 pb-2 font-semibold text-primary hover:text-primary cursor-pointer border-b-2 whitespace-nowrap"
+          >
+            {tab.name}
+          </a>
+        ))}
+      </div>
+      <div>
+        {product.tabs.map((tab) => (
+          <div className="relative mt-7" key={tab.name}>
+            <div className="absolute -top-48" id={tab.name}></div>
+            <h3 className="font-bold text-lg leading-9">{tab.name}</h3>
+            <p
+              className="whitespace-pre-wrap"
+              dangerouslySetInnerHTML={{ __html: tab.content }}
+            ></p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
--- next/components/index/product-details/components/product-image.tsx
+++ next/components/index/product-details/components/product-image.tsx
@@ -1,60 +1,68 @@
 import { MutableRefObject, useEffect, useRef, useState } from "react";
 import { useProductDetailsContext } from "../providers/product-details-provider";
-import ReactImageMagnify from 'react-image-magnify';
-import useScreen from './../../../../lib/hooks/useScreen';
-
+import ReactImageMagnify from "react-image-magnify";
+import useScreen from "./../../../../lib/hooks/useScreen";
 
 export function ProductImage(props) {
+  const { product } = useProductDetailsContext();
 
-    const { product } = useProductDetailsContext()
+  const [image, setImage] = useState("");
+  const [dimension, setDimension] = useState(0);
+  const ref: MutableRefObject<HTMLImageElement> = useRef();
 
-    const [image, setImage] = useState('');
-    const [dimension, setDimension] = useState(0);
-    const ref: MutableRefObject<HTMLImageElement> = useRef()
-
-    useEffect(() => {
-        if (product) {
-            setImage(product.image || '/assets/img/default.png')
-        }
-    }, [product]);
-
-    useEffect(() => {
-        if (ref.current.width && ref.current.height) {
-            setDimension(ref.current.width / ref.current.height)
-        } else {
-            setDimension(1)
-        }
-    }, [ref.current]);
-
-    const screenMd = useScreen('md')
-
-    const handleChangeImage = (img, index) => {
-        setImage(img);
-        let elementImage = document.getElementsByClassName("image-item")[index];
-        elementImage.scrollIntoView({behavior: "smooth", block: "nearest", inline: "center"});
+  useEffect(() => {
+    if (product) {
+      setImage(product.image || "/assets/img/default.png");
     }
+  }, [product]);
 
-    return <>
-        <img className="hidden" src={image} ref={ref} onError={(e)=>{setImage("/assets/img/default.png")}}/>
-        {
-            dimension && <ReactImageMagnify {...{
-                smallImage: {
-                    alt: '',
-                    isFluidWidth: true,
-                    src: image,
-                },
-                largeImage: {
-                    src: image,
-                    width: 1200,
-                    height: 1200 / dimension
-                },
-                enlargedImagePosition: screenMd ? 'beside': 'over',
-                className: 'border border-gray-200 rounded',
-                enlargedImageContainerClassName: 'bg-white z-20 border border-gray-200',
-                enlargedImageClassName: 'max-w-none',
-            }} />
-        }      
-         {/* <div className="image-wrapper rounded ratio-16-9 contain">
+  useEffect(() => {
+    if (ref.current.width && ref.current.height) {
+      setDimension(ref.current.width / ref.current.height);
+    } else {
+      setDimension(1);
+    }
+  }, [ref.current]);
+
+  const screenMd = useScreen("md");
+
+  const handleChangeImage = (img, index) => {
+    setImage(img);
+    let elementImage = document.getElementsByClassName("image-item")[index];
+    elementImage.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "center" });
+  };
+
+  return (
+    <>
+      <img
+        className="hidden"
+        src={image}
+        ref={ref}
+        onError={(e) => {
+          setImage("/assets/img/default.png");
+        }}
+      />
+      {dimension && (
+        <ReactImageMagnify
+          {...{
+            smallImage: {
+              alt: "",
+              isFluidWidth: true,
+              src: image,
+            },
+            largeImage: {
+              src: image,
+              width: 1200,
+              height: 1200 / dimension,
+            },
+            enlargedImagePosition: screenMd ? "beside" : "over",
+            className: "border border-gray-200 rounded",
+            enlargedImageContainerClassName: "bg-white z-20 border border-gray-200",
+            enlargedImageClassName: "max-w-none",
+          }}
+        />
+      )}
+      {/* <div className="image-wrapper rounded ratio-16-9 contain">
             <img src={image || "/assets/img/default.png"}
                 onError={(e)=>{(e.target as any).src="/assets/img/default.png"}}
             />
@@ -67,7 +75,7 @@ export function ProductImage(props) {
                 </div>
             }
         </div> */}
-        {/* <div className="list-slider-image w-full flex mt-4 overflow-y-scroll h-36 ">
+      {/* <div className="list-slider-image w-full flex mt-4 overflow-y-scroll h-36 ">
                 {
                     props.product.slide_img.map((item, index) => {
                         return  <>
@@ -80,6 +88,7 @@ export function ProductImage(props) {
                         </>;
                     })
                 }
-        </div> */}  
-    </>;
+        </div> */}
+    </>
+  );
 }
