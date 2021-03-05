63d1d577c6a43f814d912387772c65a43d5e1623
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2021-03-05 09:57:06 +0700

    Fix: Lỗi không build được

--- next/components/index/profile-user/components/form-profile.tsx
+++ next/components/index/profile-user/components/form-profile.tsx
@@ -6,77 +6,77 @@ import { useAuth } from "../../../../lib/providers/auth-provider";
 interface PropsType extends ReactProps {
   [x: string]: any;
   name?: string;
-  id?:string;
-  defaultValue?:string,
-  listOptions: any[],
+  id?: string;
+  defaultValue?: string;
+  listOptions?: any[];
 }
 
-export function FormProfile({ 
+export function FormProfile({
   name = "typeStore",
   id = "TypeStore",
   defaultValue = "",
-  listOptions = [ { type: 1, value: "Phòng khám" } , { type: 2, value: "Nhà thuốc" }, { type: 3, value: "Trình dược viên" } ],
-  ...props 
-}:PropsType) {
-    const { user } = useAuth();
-    const [userA,setUserA] = useState(null);
-    useEffect(() => {
-      setUserA(user)
+  listOptions = [
+    { type: 1, value: "Phòng khám" },
+    { type: 2, value: "Nhà thuốc" },
+    { type: 3, value: "Trình dược viên" },
+  ],
+  ...props
+}: PropsType) {
+  const { user } = useAuth();
+  const [userA, setUserA] = useState(null);
+  useEffect(() => {
+    setUserA(user);
+  }, [user]);
+  console.log(userA);
 
-    }, [user]);
-    console.log(userA);
-    
+  const [valueTypeStore, setValueTypeStore] = useState(defaultValue);
+  const [valueNameStore, setValueNameStore] = useState(null);
+  const handleChange = (id: string, value: any) => {
+    switch (id) {
+      // case "dateOfBirth":{
+      //     setUserP({...user,birthday:value});
+      //     break;
+      // }
 
-    const [valueTypeStore,setValueTypeStore] = useState(defaultValue);
-    const [valueNameStore,setValueNameStore] = useState(null);
-    const handleChange=(id:string,value:any)=>{
-        switch (id) {
-            // case "dateOfBirth":{
-            //     setUserP({...user,birthday:value});
-            //     break;
-            // }
-                
-            case "username":{
-                setUserA({...userA, username:value});
-                break;
-            }
-            case "email":{
-              setUserA({...userA, email:value});
-              break;
-            }
-            case "phone":{
-              setUserA({...userA, phone:value});
-              break;
-            }
+      case "username": {
+        setUserA({ ...userA, username: value });
+        break;
+      }
+      case "email": {
+        setUserA({ ...userA, email: value });
+        break;
+      }
+      case "phone": {
+        setUserA({ ...userA, phone: value });
+        break;
+      }
 
-            case "typeStore":{
-              setValueTypeStore(value);
-              break;
-            }
+      case "typeStore": {
+        setValueTypeStore(value);
+        break;
+      }
 
-            case "nameStore":{
-              setValueNameStore(value);
-              break;
-            }
-                
-            // case "phone":{
-            //     setUserP({...user,phone:value});
-            //     break; 
-            // }
+      case "nameStore": {
+        setValueNameStore(value);
+        break;
+      }
 
-                
-            default:
-                break;
-        }
+      // case "phone":{
+      //     setUserP({...user,phone:value});
+      //     break;
+      // }
+
+      default:
+        break;
     }
-    // console.log(user);
-    
-    
-    
-    
-    return <>
-    {
-        userA ? <>
+  };
+  // console.log(user);
+
+  return (
+    <>
+      {
+        userA ? (
+          <>
             <div className="w-11/12 lg:w-full text-16 sm:text-20 text-gray-700">
               <h3 className="uppercase border-gray-200 border-b-4 pb-2 mb-4 text-24 hidden sm:block text-left">
                 Thông tin tài khoản
@@ -117,23 +117,30 @@ export function FormProfile({
                     <div className="sm:flex justify-between items-center pt-4">
                       <p className="w-full sm:w-1/4 xl:w-2/6 xl:pr-2">Ngày sinh</p>
                       <div className="w-full sm:w-3/4 flex space-x-2 xl:w-4/6">
-                        <DateTime dateOfBirth={new Date()} handleChange={handleChange }/>
+                        <DateTime dateOfBirth={new Date()} handleChange={handleChange} />
                       </div>
                     </div>
                     <div className="sm:flex justify-between items-center pt-4">
                       <p className="w-full sm:w-1/4 xl:w-2/6 xl:pr-2">Loại cửa hàng</p>
-                      <select className="btn-outline w-full sm:w-3/4 xl:w-4/6 h-12 text-16 sm:text-20 px-0 sm:pl-2" 
-                          name={name} 
-                          value={valueTypeStore} 
-                          id={id} 
-                          onChange={(e)=>handleChange("typeStore",e.target.value)}>
-                          {
-                              listOptions.map((item,index)=>{
-                                  return <option className={item===defaultValue?"bg-primary-light":""} key={index} value={item.type}>{item.value}</option>
-                              })
-                          }
+                      <select
+                        className="btn-outline w-full sm:w-3/4 xl:w-4/6 h-12 text-16 sm:text-20 px-0 sm:pl-2"
+                        name={name}
+                        value={valueTypeStore}
+                        id={id}
+                        onChange={(e) => handleChange("typeStore", e.target.value)}
+                      >
+                        {listOptions.map((item, index) => {
+                          return (
+                            <option
+                              className={item === defaultValue ? "bg-primary-light" : ""}
+                              key={index}
+                              value={item.type}
+                            >
+                              {item.value}
+                            </option>
+                          );
+                        })}
                       </select>
-                      
                     </div>
                     {/* <div className="justify-between items-center pt-4 sm:h-12">
                       <p className="w-full sm:w-1/4">Giới tính</p>
@@ -141,17 +148,16 @@ export function FormProfile({
                         <Gender gender={user.userRef} />
                       </div>
                     </div> */}
-                    
+
                     <div className="sm:flex justify-between items-center pt-4">
                       <p className="w-full sm:w-1/4 xl:w-2/6 xl:pr-2">Tên cửa hàng</p>
                       <input
                         className="form-input w-full sm:w-3/4 xl:w-4/6 text-16 sm:text-20"
                         value={valueNameStore}
                         onChange={(e) => {
-                          handleChange("nameStore", e.target.value)
+                          handleChange("nameStore", e.target.value);
                         }}
                       />
-                      
                     </div>
                     <div className="sm:flex justify-between items-center pt-4">
                       <p className="w-full sm:w-1/4 xl:w-2/6 xl:pr-2">Mật khẩu</p>
@@ -172,7 +178,7 @@ export function FormProfile({
                     <img
                       src={user.imageLink || "/assets/img/avatar.svg"}
                       onError={(e) => {
-                        (e.target as any).src = "/assets/img/avatar.svg"
+                        (e.target as any).src = "/assets/img/avatar.svg";
                       }}
                       className="w-9/12 sm:w-6/12 md:w-6/12 lg:w-7/12 xl:w-11/12 rounded-full"
                       alt="avatar"
@@ -193,7 +199,7 @@ export function FormProfile({
               </div>
             </div>
           </>
-         : (
+        ) : (
           ""
         )
         //             <Link href="/login">
@@ -203,5 +209,6 @@ export function FormProfile({
         //             </Link>
         // }
       }
-    </>;
+    </>
+  );
 }
