8fa153351640e408c2120141d030b7019045a907
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2021-03-05 11:36:12 +0700

    Feat: [Backend] Trả về nhiếu size hình để sử dung hình ảnh sản phẩm tối ưu nhất

--- src/helpers/arito/arito.helper.ts
+++ src/helpers/arito/arito.helper.ts
@@ -32,8 +32,8 @@ export class AritoHelper {
       CacheHelper.set("arito-image-token", res.data.value);
     });
   }
-  static getImageLink(imageId: string) {
-    return `${this.host}/GetImageFile350/${imageId}/${this.imageToken}`;
+  static getImageLink(imageId: string, size: 200 | 350 | 576 | 1024 = 350) {
+    return `${this.host}/GetImageFile${size}/${imageId}/${this.imageToken}`;
   }
   static getAvatarLink(imageId: string) {
     return `${this.host}/DownloadFile0/${imageId}/${this.imageToken}`;
