8fd0c94c5c60afc86687e6d0cb36df778069d3d9
Link Thai <linkthai1995@gmail.com>
2021-03-10 17:59:41 +0700

    UPDATE: Add sliding news

--- next/layouts/default-layout/head-seo.tsx
+++ next/layouts/default-layout/head-seo.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
 export function HeadSEO({ title }) {
-    return (
-        <Head>
-            <title>{title}</title>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width, maximum-scale=1.0, user-scalable=0" />
-            <link rel="icon" href="/favicon.ico" />
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700&display=swap" rel="stylesheet"/>
-        </Head>
-    )
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta
+        name="viewport"
+        content="initial-scale=1.0, width=device-width, maximum-scale=1.0, user-scalable=0"
+      />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+      />
+    </Head>
+  );
 }
