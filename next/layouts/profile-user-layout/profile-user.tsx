8a3d7435499abc168ebbbe4c726491c5caa4aba5
ThanhNam2905 <thanhnam290596@gmail.com>
2021-03-12 09:44:05 +0700

    Fix: get Data trang Order-Detail

--- next/layouts/profile-user-layout/profile-user.tsx
+++ next/layouts/profile-user-layout/profile-user.tsx
@@ -1,58 +1,11 @@
 import Link from "next/link";
-<<<<<<< HEAD
-import { IoNotificationsOutline } from "react-icons/io5";
-=======
 import { useRouter } from "next/router";
 import { AiOutlineBell } from "react-icons/ai";
->>>>>>> 9402c26b7e295cd9d8a041c722580860d083d0aa
 import { BiDonateHeart, BiListPlus } from "react-icons/bi";
 import { ImUser } from "react-icons/im";
 
 export function ProfileUser() {
   const router = useRouter();
-<<<<<<< HEAD
-  const { user, saveCurrentPath } = useAuth();
-  useEffect(() => {
-    if (user === null) {
-      sessionStorage.setItem(LOGIN_PATHNAME, router.pathname);
-      router.replace("/login");
-    }
-  }, [user]);
-  // console.log(user);
-
-  return (
-    <>
-      {user ? (
-        <>
-          {/* <div className="hidden lg:flex items-center justify-center lg:justify-start mb-3 md:mb-0">
-                <img src={ user.imageLink || "/assets/img/avatar.svg" } onError={(e) => {(e.target as any).src="/assets/img/avatar.svg"}} alt="" className="lg:w-1/4 rounded-full" />
-                <div className=" ml-3 text-sm md:text-base">
-                    <a href="#" className="block font-normal mb-1 md:mb-2">
-                        { user.nickname }
-                    </a>
-                    <a href="/profile" className="block text-gray-500">
-                        Chỉnh sửa hồ sơ
-                    </a>
-                </div>
-            </div> */}
-          <ul className="flex flex-col md:flex-row lg:flex-col md:justify-between lg:mt-4 mb-5 sm:mb-3 md:mb-0 w-full">
-            <li className="flex md:mb-5 xl:mb-7 items-center py-2.5 md:py-0 border-t border-b md:border-0 pl-5 md:pl-0">
-              <ImUser className="text-primary border-2 border-primary rounded-full text-md sm:text-lg md:text-2xl p-0.5 mr-2" />
-              <Link href="/profile" shallow={true}>
-                <a
-                  className={`"  uppercase text-sm md:text-base hover:text-primary font-extralight " 
-                                ${router.pathname == "/profile" ? " text-primary" : ""}`}
-                >
-                  Tài khoản của tôi
-                </a>
-              </Link>
-            </li>
-            <li className="flex md:mb-5 xl:mb-7 items-center py-2.5 md:py-0 border-b md:border-b-0 pl-5 md:pl-0">
-              <BiListPlus className="text-primary border-2 border-primary rounded text-md sm:text-lg md:text-2xl mr-2" />
-              <Link href="/profile/order-history" shallow={true}>
-                <a
-                  className={`"  uppercase text-sm md:text-base hover:text-primary font-extralight "
-=======
 
   return (
     <>
@@ -73,55 +26,12 @@ export function ProfileUser() {
           <Link href="/profile/order-history" shallow={true}>
             <a
               className={`"  uppercase text-sm md:text-base hover:text-primary font-extralight "
->>>>>>> 9402c26b7e295cd9d8a041c722580860d083d0aa
                                 ${
                                   router.pathname == "/profile/order-history" ||
                                   router.pathname == "/profile/order-details"
                                     ? " text-primary"
                                     : ""
                                 }`}
-<<<<<<< HEAD
-                >
-                  Đơn hàng
-                </a>
-              </Link>
-            </li>
-            <li className="flex md:mb-5 xl:mb-7 items-center py-2.5 md:py-0 border-b md:border-b-0 pl-5 md:pl-0">
-              <IoNotificationsOutline className="text-primary text-md sm:text-lg md:text-2xl mr-2" />
-              <Link href="/profile/notification" shallow={true}>
-                <a
-                  className={`"  uppercase text-sm md:text-base hover:text-primary font-extralight "
-                                ${
-                                  router.pathname == "/profile/notification" ? " text-primary" : ""
-                                }`}
-                >
-                  Thông báo
-                </a>
-              </Link>
-            </li>
-            <li className="flex md:mb-5 xl:mb-7 items-center py-2.5 md:py-0 border-b md:border-b-0 pl-5 md:pl-0">
-              <BiDonateHeart className="text-primary text-md sm:text-lg md:text-2xl mr-2" />
-              <Link href="/profile/reward-point" shallow={true}>
-                <a
-                  className={`"  uppercase text-sm md:text-base hover:text-primary font-extralight "
-                                ${
-                                  router.pathname == "/profile/reward-point" ? " text-primary" : ""
-                                }`}
-                >
-                  Điểm thưởng
-                </a>
-              </Link>
-            </li>
-          </ul>
-        </>
-      ) : (
-        <Link href="/login">
-          <a className="btn-default h-12" onClick={saveCurrentPath}>
-            Đăng nhập
-          </a>
-        </Link>
-      )}
-=======
             >
               Đơn mua
             </a>
@@ -154,7 +64,6 @@ export function ProfileUser() {
           </Link>
         </li>
       </ul>
->>>>>>> 9402c26b7e295cd9d8a041c722580860d083d0aa
     </>
   );
 }
