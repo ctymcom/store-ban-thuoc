34c2612cb0874c30fc6ba581981ab6aa06d8c2f8
Nin <nnninh.17ck1@gmail.com>
2021-03-17 09:34:58 +0700

    update: chức năng mua lại

--- next/lib/providers/cart-provider.tsx
+++ next/lib/providers/cart-provider.tsx
@@ -45,55 +45,59 @@ export function CartProvider({ children }: any) {
   const toast = useToast();
 
   useEffect(() => {
-    try {
-      setLoading(true);
-      let cartProductStorage = JSON.parse(
-        localStorage.getItem("cartProductStorage")
-      ) as CartProduct[];
-      if (cartProductStorage) {
-        ProductService.getAll({
-          query: {
-            limit: 0,
-            filter: {
-              _id: { __in: cartProductStorage.map((x) => x.productId) },
+    if (!cartProducts) {
+      try {
+        setLoading(true);
+        let cartProductStorage = JSON.parse(
+          localStorage.getItem("cartProductStorage")
+        ) as CartProduct[];
+        if (cartProductStorage) {
+          ProductService.getAll({
+            query: {
+              limit: 0,
+              filter: {
+                _id: { __in: cartProductStorage.map((x) => x.productId) },
+              },
             },
-          },
-        }).then((res) => {
-          setLoading(false);
+          }).then((res) => {
+            setLoading(false);
 
-          cartProductStorage.forEach((cartProduct) => {
-            let product = res.data.find((x) => x.id == cartProduct.productId);
-            if (product) {
-              cartProduct.price = product.salePrice;
-              cartProduct.amount = product.salePrice * cartProduct.qty;
-              cartProduct.product = product;
-            }
+            cartProductStorage.forEach((cartProduct) => {
+              let product = res.data.find((x) => x.id == cartProduct.productId);
+              if (product) {
+                cartProduct.price = product.salePrice;
+                cartProduct.amount = product.salePrice * cartProduct.qty;
+                cartProduct.product = product;
+              }
+            });
+            cartProductStorage = cartProductStorage.filter((x) => x.product);
+            setLoading(false);
+            setCartProductCount(
+              cartProductStorage.reduce((count, cartProduct) => (count += cartProduct.qty), 0)
+            );
+            setCartTotal(
+              cartProductStorage.reduce(
+                (total, cartProduct) =>
+                  cartProduct.active ? (total += cartProduct.amount) : total,
+                0
+              )
+            );
           });
-          cartProductStorage = cartProductStorage.filter((x) => x.product);
+          setCartProducts([...cartProductStorage]);
+          console.log(cartProductStorage);
+        } else {
+          console.log(cartProductStorage);
           setLoading(false);
-          setCartProductCount(
-            cartProductStorage.reduce((count, cartProduct) => (count += cartProduct.qty), 0)
-          );
-          setCartTotal(
-            cartProductStorage.reduce(
-              (total, cartProduct) => (cartProduct.active ? (total += cartProduct.amount) : total),
-              0
-            )
-          );
-        });
-        setCartProducts([...cartProductStorage]);
-        console.log(cartProductStorage);
-      } else {
-        console.log(cartProductStorage);
+        }
+      } catch (error) {
+        console.log(error);
+        setPromotion("");
         setLoading(false);
       }
-    } catch (error) {
-      console.log(error);
-      setPromotion("");
-      setLoading(false);
     }
   }, []);
   useEffect(() => {
+    console.log(cartProducts);
     localStorage.setItem(
       "cartProductStorage",
       JSON.stringify(
@@ -112,20 +116,57 @@ export function CartProvider({ children }: any) {
       )
     );
   }, [cartProducts]);
-  const reOrder = async (items: CartProduct[]) => {
-    items.forEach((item) => {
-      if (item.productId) {
-        ProductService.getOne({ id: item.productId }).then((res) => {
-          let { a, b, ...product } = res;
-          addProductToCart(product, item.qty);
+  const reOrder = async (items: [{ productId: string; qty: number }]) => {
+    let resCartProducts = [...items];
+    let newCarts = cartProducts;
+    newCarts.forEach((item) => (item.active = false));
+    console.log(resCartProducts);
+
+    if (resCartProducts) {
+      ProductService.getAll({
+        query: {
+          limit: 0,
+          filter: {
+            _id: { __in: resCartProducts.map((x) => x.productId) },
+          },
+        },
+      }).then((res) => {
+        console.log(res);
+
+        setLoading(false);
+
+        resCartProducts.forEach((reCartProduct) => {
+          let { __typename, ...product } = res.data.find((x) => x.id == reCartProduct.productId);
+          if (product) {
+            let cartFound = newCarts.find((x) => x.productId == product.id);
+            if (cartFound) {
+              cartFound.active = true;
+              cartFound.qty = reCartProduct.qty;
+              cartFound.price = product.salePrice;
+              cartFound.amount = product.salePrice * reCartProduct.qty;
+            } else {
+              let cartResPro: CartProduct = {
+                productId: reCartProduct.productId,
+                price: product.salePrice,
+                qty: reCartProduct.qty,
+                amount: product.salePrice * reCartProduct.qty,
+                product,
+                active: true,
+              };
+              newCarts.push(cartResPro);
+            }
+          }
         });
-      }
-      console.log(cartProducts);
-    });
+        setLoading(false);
+      });
+      await setCartProducts([...newCarts]);
+      console.log(newCarts);
+    } else {
+      setLoading(false);
+    }
   };
   const addProductToCart = (product: Product, qty: number): boolean => {
     if (!qty) return false;
-
     let cartProduct = cartProducts.find((x) => x.productId == product.id);
     if (cartProduct) {
       cartProduct.qty += qty;
