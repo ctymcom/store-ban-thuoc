114fc77c32ede6da33c5ba821e79f6f7faea8f03
Nin <nnninh.17ck1@gmail.com>
2021-03-17 15:20:25 +0700

    update: thêm danh sách sản phẩm, dùng <Textarea/>

---
+++ next/components/index/checkout/components/list-cart-checkout.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useCart, CartProduct } from "../../../../lib/providers/cart-provider";
+import { Spinner } from "../../../shared/utilities/spinner";
+import { HiOutlineShoppingCart } from "react-icons/hi";
+import { NumberPipe } from "../../../../lib/pipes/number";
+interface PropsType extends ReactProps {
+  title: string;
+}
+const ListCartCheckout = (props: PropsType) => {
+  const { cartProducts } = useCart();
+  return cartProducts ? (
+    <div className={`${props.className || ""}`}>
+      <h3 className=" border-b-2 pb-2 flex gap-2 text-16 uppercase items-center">
+        <HiOutlineShoppingCart className="text-primary text-20" /> {props.title}
+      </h3>
+      {cartProducts.map((item: CartProduct, index) => {
+        if (item.active) {
+          return (
+            <div className="flex-wrap flex justify-between text-14 leading-7">
+              <p className="whitespace-nowrap">
+                {item.product.name} <span className="font-bold">X</span> {item.qty}
+                <span className="ml-5">-</span>
+              </p>
+              <p>{NumberPipe(item.price, false)} VND</p>
+            </div>
+          );
+        }
+      })}
+    </div>
+  ) : (
+    <Spinner />
+  );
+};
+
+export default ListCartCheckout;
