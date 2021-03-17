e8bcb3dd0ccac4ccf67658e354edda7b9f19eec6
Nin <nnninh.17ck1@gmail.com>
2021-03-17 15:26:07 +0700

    update: thêm danh sách sản phẩm, dùng <Textarea/>

--- next/components/index/checkout/components/list-cart-checkout.tsx
+++ next/components/index/checkout/components/list-cart-checkout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useCart, CartProduct } from "../../../../lib/providers/cart-provider";
 import { Spinner } from "../../../shared/utilities/spinner";
-import { HiOutlineShoppingCart } from "react-icons/hi";
+import { HiCheck, HiOutlineShoppingCart } from "react-icons/hi";
 import { NumberPipe } from "../../../../lib/pipes/number";
 interface PropsType extends ReactProps {
   title: string;
@@ -17,7 +17,8 @@ const ListCartCheckout = (props: PropsType) => {
         if (item.active) {
           return (
             <div className="flex-wrap flex justify-between text-14 leading-7">
-              <p className="whitespace-nowrap">
+              <p className="whitespace-nowrap flex items-center">
+                <HiCheck className="text-primary pr-1" />
                 {item.product.name} <span className="font-bold">X</span> {item.qty}
                 <span className="ml-5">-</span>
               </p>
