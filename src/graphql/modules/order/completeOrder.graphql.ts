e703d8ccecc61aa26bd853a3897db826b545431e
Hoang Minh <nhminh@apcs.vn>
2021-03-31 17:19:09 +0700

    checkout

---
+++ src/graphql/modules/order/completeOrder.graphql.ts
@@ -0,0 +1,26 @@
+import { gql } from "apollo-server-express";
+import { ROLES } from "../../../constants/role.const";
+import { AritoHelper } from "../../../helpers/arito/arito.helper";
+import { Context } from "../../context";
+import { OrderModel } from "./order.model";
+
+export default {
+  schema: gql`
+    extend type Mutation {
+      completeOrder(orderId: ID!): String
+    }
+  `,
+  resolver: {
+    Mutation: {
+      completeOrder: async (root: any, args: any, context: Context) => {
+        context.auth(ROLES.ADMIN_EDITOR_MEMBER_CUSTOMER);
+        const { orderId } = args;
+        const order =await OrderModel.findById(orderId);
+        if (!order) {
+          throw Error();
+        }
+        return AritoHelper.completeOrder(order);
+      }
+    },
+  },
+};
