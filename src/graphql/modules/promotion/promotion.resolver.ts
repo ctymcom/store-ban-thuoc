f5972c624bed990cc52a3a4a9dc3ec75447c6075
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2021-04-09 19:00:22 +0700

    Fix: Láy danh sách khuyến mãi từ token ngừoi dùng

--- src/graphql/modules/promotion/promotion.resolver.ts
+++ src/graphql/modules/promotion/promotion.resolver.ts
@@ -1,10 +1,16 @@
-import { ROLES } from "../../../constants/role.const";
-import { AuthHelper } from "../../../helpers";
+import { set } from "lodash";
+
+import { AritoHelper } from "../../../helpers/arito/arito.helper";
 import { Context } from "../../context";
 import { promotionService } from "./promotion.service";
 
 const Query = {
   getAllPromotion: async (root: any, args: any, context: Context) => {
+    if (context.isAuth) {
+      const res = await AritoHelper.getAllPromotion(1, null, context.tokenData.ref);
+      const codes = res.data.map((d) => d.code);
+      set(args, "q.filter.code", { $in: codes });
+    }
     return promotionService.fetch(args.q);
   },
   getOnePromotion: async (root: any, args: any, context: Context) => {
