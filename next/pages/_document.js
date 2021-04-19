2c992a64b9667d1a57328f08d0226fdbf1acd2dd
Link Thai <linkthai1995@gmail.com>
2021-04-19 16:35:41 +0700

    ADD: gtag + messenger + dmca + FIX: countdown

--- next/pages/_document.js
+++ next/pages/_document.js
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+const isProduction = process.env.NODE_ENV === "production";
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -14,13 +16,18 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
           <div id="dialog-root"></div>
-              
-          <div id="fb-root"></div>
-          <div
-            class="fb-customerchat"
-            language="vi" themeColor="#42B54A" pageId="102164275124516"
-          ></div>
+          {isProduction && (
+            <>
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-H8HTX7NFE1"></script>
+              <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments)}
+                gtag('js', new Date());
 
+                gtag('config', 'G-H8HTX7NFE1');
+              </script>
+            </>
+          )}
         </body>
       </Html>
     )
