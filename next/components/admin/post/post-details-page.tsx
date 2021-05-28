82d029636806b94e077272cec7863b4abb2f456d
Link Thai <linkthai1995@gmail.com>
2021-05-28 16:26:00 +0700

    ADD: title post details

--- next/components/admin/post/post-details-page.tsx
+++ next/components/admin/post/post-details-page.tsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 import { Editor } from "../../shared/utilities/form/editor";
+import { Input } from "../../shared/utilities/form/input";
 import { Spinner } from "../../shared/utilities/spinner";
 import { PostDetailsForm } from "./component/post-details-form";
-import { usePostDetailsContext } from "./providers/post-details-provider";
+import { PostDetailsContext, usePostDetailsContext } from "./providers/post-details-provider";
 
 interface PropTypes extends ReactProps {}
 export function PostDetailsPage(props: PropTypes) {
   const { post } = usePostDetailsContext();
-  const [title, setTitle] = useState("");
+  const [title, setTitle] = useState<string>();
   const [content, setContent] = useState<any>();
 
   useEffect(() => {
     if (post) {
-      setTitle(post.id ? "Cập nhật bài đăng" : "Tạo bài đăng mới");
+      setTitle(post.title);
       setContent(post.content);
     }
   }, [post]);
@@ -21,19 +22,43 @@ export function PostDetailsPage(props: PropTypes) {
     if (content !== undefined) {
       post.content = content;
     }
-  }, [content]);
+    if (title !== undefined) {
+      post.title = title;
+    }
+  }, [content, title]);
 
   return (
     <>
       <div className="flex justify-between items-center h-12">
-        <div className="text-xl text-gray-800 font-semibold">{title}</div>
+        <div className="text-xl text-gray-800 font-semibold">
+          {post ? (post.id ? "Cập nhật bài đăng" : "Tạo bài đăng mới") : ""}
+        </div>
       </div>
       {!post ? (
         <Spinner />
       ) : (
         <div className="flex max-w-7xl pt-2">
-          <div className="flex-grow">
-            <Editor minHeight="850px" maxHeight="850px" value={content} onChange={setContent} />
+          <div className="flex-grow bg-white v-scrollbar p-4" style={{ height: "850px" }}>
+            <PostDetailsContext.Consumer>
+              {({ post }) => (
+                <>
+                  <Input
+                    className="text-2xl border-0 focus:shadow-none font-semibold mb-2"
+                    placeholder="Tiêu đề bài viết"
+                    value={title}
+                    onChange={setTitle}
+                  />
+                  <Editor
+                    controlClassName=""
+                    noBorder
+                    noFocus
+                    placeholder="Nội dung bài viết"
+                    value={content}
+                    onChange={setContent}
+                  />
+                </>
+              )}
+            </PostDetailsContext.Consumer>
           </div>
           <div className="flex-grow-0 w-96">
             <PostDetailsForm />
